60a4cef97bfd92f0a0188545b9568182b2880817
Redanmax <reberamirez99@gmail.com>
2023-10-19 20:00:35 -0600

    Documentacion generada de Swagger 8.5
    Modificacion de route y controladores

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API de Redanmax",
+ *      description="API de ejempo para clase de desarrollo de Backend y APIs",
+ *      @OA\Contact(
+ *          email="reberamirez99@gmail.com",
+ *          name="Rebeca Carolina Ramirez Rodriguez",
+ *          url="https://localhost/api/"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/respuestaController.php
+++ app/Http/Controllers/respuestaController.php
@@ -6,8 +6,26 @@ use Illuminate\Http\Request;
 
 class respuestaController extends Controller
 {
-    //
-
+    //   
+/**
+ * 
+ * Descripcion de prueba para la clase de desarrollo de backend y API desarrollada por todo el equipo 19/10/2023
+ * @return \Illuminate\Http\Response
+ *
+ * @OA\Get(
+ *     path="/api/ejecutar",
+ *     tags={"ejecutar"},
+ *     summary="Este es un endpoint o metodo para obtener datos y mostrarlos al usuario",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Se devuelve todos los registros cuando la consulta sea satisfecha"
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="No se pudo realizar la ejecución al parecer hubo un error interno"
+ *     )
+ * ) 
+ */
     public function index (){
 
         return response()->json([
